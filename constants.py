1613c820cbfe93db4a986557705153957d0b5257
Riten Patel <108758148+gOeY2@users.noreply.github.com>
2022-10-23 17:14:44 -0400

    Add files via upload

---
+++ constants.py
@@ -0,0 +1,13 @@
+DOMAIN = "http://127.0.0.1:5000"
+
+# noinspection SpellCheckingInspection
+GOOGLE_CLIENT_ID = (
+    "526064511697-g8ehfcclqnbar3uqgq44uq55d07aflsn.apps.googleusercontent.com"
+)
+
+GOOGLE_API_SCOPES = [
+    "https://www.googleapis.com/auth/documents",
+    "https://www.googleapis.com/auth/userinfo.profile",
+]
+
+OPENAI_API_KEY = "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"
